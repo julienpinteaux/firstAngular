2700c34a6a9d4377055e158c52fdd6b2823dea83
DESKTOP-ID3VLKC\Julien <juju.om26@gmail.com>
2017-11-29 13:29:07 +0100

    modification objet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 
+class Personne{
+	nom : string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my App';
-  nom = "Einsten"
-  prenom = "Albert"
-  description = "Albert Einstein (prononcé en allemand [ˈalbɐt ˈaɪnʃtaɪn] Prononciation du titre dans sa version originale Écouter) né le 14 mars 1879N 1 à Ulm, dans le Wurtemberg, et mort le 18 avril 1955 à Princeton, dans le New Jersey, est un physicien théoricien. Il fut successivement allemand, apatride (1896), suisse (1901) et de double nationalité helvético-américaine (1940)1. Il fut le camarade d'études de Mileva Marić, puis, en première noce, son époux. Il fut le cousin d'Elsa Einstein, dont il fut, en seconde noce, l'époux."
+	
+	title = 'my App';
+	
+	personne {
+	
+	  
+	  nom : "Einsten",
+	  prenom : "Albert",
+	  description : "Albert Einstein (prononcé en allemand [ˈalbɐt ˈaɪnʃtaɪn] Prononciation du titre dans sa version originale Écouter) né le 14 mars 1879N 1 à Ulm, dans le Wurtemberg, et mort le 18 avril 1955 à Princeton, dans le New Jersey, est un physicien théoricien. Il fut successivement allemand, apatride (1896), suisse (1901) et de double nationalité helvético-américaine (1940)1. Il fut le camarade d'études de Mileva Marić, puis, en première noce, son époux. Il fut le cousin d'Elsa Einstein, dont il fut, en seconde noce, l'époux."
+	}
 }
